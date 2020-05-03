e10060ce97a0623382209dfd9a107f2659d1ea4e
Cole Arendt <cole.arendt@outlook.com>
2020-05-02 22:31:29 -0400

    show example of broken nestedness

---
+++ simple-nested/app.R
@@ -0,0 +1,75 @@
+
+library(shiny)
+library(htmltools)
+
+mod3ui <- function(id) {
+    ns <- NS(id)
+
+    tagList(
+        uiOutput(ns("hm")),
+        uiOutput("1-1-2-1-1-2-3-hm")
+    )
+}
+
+mod3 <- function(input,output,session) {
+    ns <- session$ns
+    browser()
+
+    output$hm <- renderUI({
+        browser()
+        tagList(
+            tags$b("Three"),
+            tags$br()
+        )
+    }
+    )
+}
+mod2ui <- function(id) {
+    ns <- NS(id)
+
+    tagList(
+        tags$b("Two"),
+        tags$br(),
+        mod3ui(ns("3"))
+    )
+}
+
+mod2 <- function(input,output,session) {
+    ns <- session$ns
+    callModule(mod3, ns("3"))
+}
+
+mod1ui <- function(id) {
+    ns <- NS(id)
+
+    tagList(
+        tags$b("One"),
+        tags$br(),
+        mod2ui(ns("2"))
+    )
+}
+
+mod1 <- function(input,output,session) {
+    ns <- session$ns
+    callModule(mod2, ns("2"))
+}
+
+ui <- fluidPage(
+
+    titlePanel("Old Faithful Geyser Data"),
+
+    sidebarLayout(
+        sidebarPanel(
+        ),
+
+        mainPanel(
+            mod1ui("1")
+        )
+    )
+)
+
+server <- function(input, output) {
+    callModule(mod1, "1")
+}
+
+shinyApp(ui = ui, server = server)
